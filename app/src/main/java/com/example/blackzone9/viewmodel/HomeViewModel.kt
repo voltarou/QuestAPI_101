ec0660e8a0c81ebb736176495375b4855aee61c9
voltarou <161274983+voltarou@users.noreply.github.com>
2025-09-02 15:52:33 +0700

    homeview

---
+++ app/src/main/java/com/example/blackzone9/viewmodel/HomeViewModel.kt
@@ -0,0 +1,42 @@
+package com.example.blackzone9.viewmodel
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.blackzone9.modeldata.DataSiswa
+import com.example.blackzone9.repositori.RepositoriDataSiswa
+import kotlinx.coroutines.launch
+import okio.IOException
+import retrofit2.HttpException
+
+sealed interface StatusUiSiswa {
+    data class Success(val siswa: List<DataSiswa> = listOf()) : StatusUiSiswa
+    object Error : StatusUiSiswa
+    object Loading : StatusUiSiswa
+
+}
+
+class HomeViewModel(private val repositoriDataSiswa: RepositoriDataSiswa) : ViewModel() {
+    var statusUiSiswa: StatusUiSiswa by mutableStateOf(StatusUiSiswa.Loading)
+        private set
+
+    init {
+        loadSiswa()
+    }
+
+    fun loadSiswa() {
+        viewModelScope.launch {
+            listSiswa = statusUiSiswa.loading
+            listSiswa = try {
+                statusUiSiswa.success(repositoriDataSiswa.getSiswa())
+
+            } catch (e: IOException) {
+                StatusUiSiswa.Error
+            } catch (e: HttpException) {
+                StatusUiSiswa.Error
+            }
+        }
+    }
+}
