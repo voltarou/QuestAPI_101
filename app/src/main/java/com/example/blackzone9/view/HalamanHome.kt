2ff5071b8ce76a9aca69eddad414358d2e1af480
voltarou <161274983+voltarou@users.noreply.github.com>
2025-09-02 17:00:52 +0700

    home

--- app/src/main/java/com/example/blackzone9/view/HalamanHome.kt
+++ app/src/main/java/com/example/blackzone9/view/HalamanHome.kt
@@ -16,19 +16,78 @@ import androidx.compose.material.icons.filled.Phone
 import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.blackzone9.R
 import com.example.blackzone9.modeldata.DataSiswa
+import com.example.blackzone9.uicontroller.DestinasiNavigasi
+import com.example.blackzone9.viewmodel.HomeViewModel
+import com.example.blackzone9.viewmodel.PenyediaViewModel
 import com.example.blackzone9.viewmodel.StatusUiSiswa
+
+object DestinasiHome: DestinasiNavigasi {
+    override val route = "home"
+    override val titleRes = R.string.app_name
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun HomeScreen(
+    modifier: Modifier = Modifier,
+    viewModel: HomeViewModel = viewModel(factory = PenyediaViewModel.Factory)
+) {
+    val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior()
+    Scaffold(
+        modifier = modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
+        topBar = {
+            TopAppBar(
+                title = { Text(stringResource(DestinasiHome.titleRes)) },
+                colors = TopAppBarDefaults.mediumTopAppBarColors(
+                    containerColor = MaterialTheme.colorScheme.primaryContainer
+                ),
+                scrollBehavior = scrollBehavior
+            )
+        },
+        floatingActionButton = {
+            FloatingActionButton(
+                onClick = { },
+                shape = MaterialTheme.shapes.medium,
+                modifier = Modifier.padding(
+                    dimensionResource(id = R.dimen.padding_large)
+                )
+            )
+            {
+                Icon(
+                    imageVector = Icons.Default.Phone,
+                    contentDescription = stringResource(R.string.entry_siswa)
+                )
+            }
+        },
+    ) { innerPadding ->
+        HomeBody(
+            statusUiSiswa = viewModel.statusUiSiswa,
+            retryAction = viewModel::getSiswa, // Panggilan ke getSiswa()
+            modifier = modifier
+                .padding(innerPadding)
+                .fillMaxSize()
+        )
+    }
+}
 
 @Composable
 fun HomeBody(
@@ -45,9 +104,7 @@ fun HomeBody(
             is StatusUiSiswa.Success -> DaftarSiswa(statusUiSiswa.siswa)
             is StatusUiSiswa.Error -> ErrorScreen(retryAction, modifier.fillMaxSize())
         }
-
     }
-
 }
 
 @Composable
@@ -98,7 +155,6 @@ fun ItemSiswa(
     Card(
         modifier = modifier,
         elevation = CardDefaults.cardElevation(defaultElevation = 2.dp)
-
     ) {
         Column(
             modifier = Modifier.padding(dimensionResource(id = R.dimen.padding_large)),
