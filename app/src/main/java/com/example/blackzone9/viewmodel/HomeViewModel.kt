86e37555ae4f650499f505d121cd46a4314481d3
voltarou <161274983+voltarou@users.noreply.github.com>
2025-09-02 17:00:58 +0700

    view model

--- app/src/main/java/com/example/blackzone9/viewmodel/HomeViewModel.kt
+++ app/src/main/java/com/example/blackzone9/viewmodel/HomeViewModel.kt
@@ -8,30 +8,38 @@ import androidx.lifecycle.viewModelScope
 import com.example.blackzone9.modeldata.DataSiswa
 import com.example.blackzone9.repositori.RepositoriDataSiswa
 import kotlinx.coroutines.launch
-import okio.IOException
 import retrofit2.HttpException
+import java.io.IOException
 
 sealed interface StatusUiSiswa {
     data class Success(val siswa: List<DataSiswa> = listOf()) : StatusUiSiswa
     object Error : StatusUiSiswa
     object Loading : StatusUiSiswa
-
 }
 
 class HomeViewModel(private val repositoriDataSiswa: RepositoriDataSiswa) : ViewModel() {
+    /**
+     * The mutable State that stores the status of the most recent request
+     */
     var statusUiSiswa: StatusUiSiswa by mutableStateOf(StatusUiSiswa.Loading)
         private set
 
+    /**
+     * Call getSiswa() on init to get Siswa data
+     */
     init {
-        loadSiswa()
+        getSiswa()
     }
 
-    fun loadSiswa() {
+    /**
+     * Gets Siswa data from the RepositoriDataSiswa API and updates the
+     * [DataSiswa] [List] [statusUiSiswa].
+     */
+    fun getSiswa() {
         viewModelScope.launch {
-            listSiswa = statusUiSiswa.loading
-            listSiswa = try {
-                statusUiSiswa.success(repositoriDataSiswa.getSiswa())
-
+            statusUiSiswa = StatusUiSiswa.Loading
+            statusUiSiswa = try {
+                StatusUiSiswa.Success(repositoriDataSiswa.getSiswa())
             } catch (e: IOException) {
                 StatusUiSiswa.Error
             } catch (e: HttpException) {
