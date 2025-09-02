4c78b5b044fd108d4ecf48488d0b005045306818
voltarou <161274983+voltarou@users.noreply.github.com>
2025-09-02 17:01:19 +0700

    main

--- app/src/main/java/com/example/blackzone9/MainActivity.kt
+++ app/src/main/java/com/example/blackzone9/MainActivity.kt
@@ -12,6 +12,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.blackzone9.ui.theme.BlackZone9Theme
+import com.example.blackzone9.uicontroller.DataSiswaApp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,8 +21,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             BlackZone9Theme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
+                    DataSiswaApp(
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
