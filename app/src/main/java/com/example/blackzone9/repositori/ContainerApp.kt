a0c48b562293aaf75266d164faa897fd356712eb
voltarou <161274983+voltarou@users.noreply.github.com>
2025-09-02 17:00:35 +0700

    container final

--- app/src/main/java/com/example/blackzone9/repositori/ContainerApp.kt
+++ app/src/main/java/com/example/blackzone9/repositori/ContainerApp.kt
@@ -7,34 +7,37 @@ import kotlinx.serialization.json.Json
 import okhttp3.MediaType.Companion.toMediaType
 import retrofit2.Retrofit
 
-interface ContainerApp{
-    val repositoriDataSiswa : RepositoriDataSiswa
+interface ContainerApp {
+    val repositoriDataSiswa: RepositoriDataSiswa
 }
 
-class DefaultContainerApp: ContainerApp{
-    private val baseurl = "http://localhost/phpmyadmin/index.php?route=/database/structure&server=1&db=tiumy&table=teman"
+class DefaultContainerApp : ContainerApp {
+    private val baseurl = "http://10.0.2.2/api/"
+
+    private val json = Json {
+        ignoreUnknownKeys = true
+        prettyPrint = true
+        isLenient = true
+    }
 
     private val retrofit: Retrofit = Retrofit.Builder()
         .baseUrl(baseurl)
-        .addConverterFactory(Json {
-            ignoreUnknownKeys = true
-            prettyPrint = true
-            isLenient = true
-        }.asConverterFactory("application/json".toMediaType()))
+        .addConverterFactory(json.asConverterFactory("application/json".toMediaType()))
         .build()
 
     private val retrofitService: ServiceApiSiswa by lazy {
-            retrofit.create(ServiceApiSiswa::class.java)
-}
+        retrofit.create(ServiceApiSiswa::class.java)
+    }
 
-    override val repositoriDataSiswa: RepositoriDataSiswa by lazy{
+    override val repositoriDataSiswa: RepositoriDataSiswa by lazy {
         JaringanRepositoriDataSiswa(retrofitService)
     }
-class AplikasiDataSiswa: Application() {
+}
+
+class AplikasiDataSiswa : Application() {
     lateinit var container: ContainerApp
     override fun onCreate() {
         super.onCreate()
-        this.container = DefaultContainerApp()
+        container = DefaultContainerApp()
     }
 }
-    }
